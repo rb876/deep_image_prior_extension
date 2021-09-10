499bcbf7f6e83f808edbba5ff6f7a65bb69ebdfe
rb876 <riccardo.barbano.19@ucl.ac.uk>
2021-09-10 15:30:18 +0100

    minor modifications

--- src/examples/jacobian_exp.py
+++ src/examples/jacobian_exp.py
@@ -1,20 +1,21 @@
 
 import torch
 import torch.nn as nn
+from torch.nn.utils import parameters_to_vector, vector_to_parameters
 from copy import deepcopy
 
 class SimpleNet(torch.nn.Module):
     """
     A 2 hidden-layer neural network
     """
-    def __init__(self):
+    def __init__(self, input_dim, out_dim, hidden_dim):
         super(SimpleNet, self).__init__()
         self.net = nn.Sequential(
-            nn.Linear(10, 25),
+            nn.Linear(input_dim, hidden_dim),
             nn.LeakyReLU(0.2),
-            nn.Linear(25, 25),
+            nn.Linear(hidden_dim, hidden_dim),
             nn.LeakyReLU(0.2),
-            nn.Linear(25, 200),
+            nn.Linear(hidden_dim, out_dim),
             nn.Tanh()
             )
         self.net.apply(self.init_weights)
@@ -47,54 +48,63 @@ def compute_jacobian_single_batch(input, model, out_dim):
         jac.append(jacs_o)
     return torch.stack(jac, dim=0)
 
-def apply_model(input, params, model):
-    nn.utils.vector_to_parameters(params, model.parameters())
+def apply_perturbed_model(input, model, omega):
+    model = deepcopy(model)
+    params = model.parameters()
+    pert_params = parameters_to_vector(params) + omega
+    vector_to_parameters(pert_params, model.parameters())
     return model(input)
 
-def compute_central_diff_approx(input, model, eps=1e-3):
+def central_diff_approx(input, model, eps=1e-6):
+    vec_params_shape = parameters_to_vector(model.parameters()).size()
+    omega = eps * torch.zeros(*vec_params_shape).normal_(0, 1)
+    f_add = apply_perturbed_model(input, model, omega)
+    f_min = apply_perturbed_model(input, model, -omega)
+    return (f_add - f_min)/2*eps
 
-    omega = eps*torch.zeros_like(nn.utils.parameters_to_vector(model.parameters())).normal_(0, 1)
-    model_1, model_2 = deepcopy(model), deepcopy(model)
-    params_add = nn.utils.parameters_to_vector(model_1.parameters()) + eps * omega
-    f_add = apply_model(input, params_add, model_1)
-    params_min = nn.utils.parameters_to_vector(model_2.parameters()) - eps * omega
-    f_min = apply_model(input, params_min, model_2)
-    return (f_add - f_min) /2*eps
+def randomised_SVD_jacobian(input, model, n_rank):
 
-def randomised_SVD_jacobian(input, model, n_rank=50):
-
+    model.eval()
     forward_maps = []
     for _ in range(n_rank):
-        forward_maps.append(compute_central_diff_approx(input, model))
+        forward_maps.append(central_diff_approx(input, model))
     forward_map = torch.cat(forward_maps).transpose(-2, -1)
     q, _ = torch.linalg.qr(forward_map)
     b_t_ = []
     for l in range(q.size()[-1]):
-        q_l = q[:, l]
+        q_l = q[:, l].detach()
         out_l = model(input) @ q_l
         model.zero_grad()
-        out_l.backward(retain_graph=True)
+        out_l.backward()
         store_device = 'cpu' if out_l.device == torch.device('cpu') else 'cuda:0'
         b_l = agregate_flatten_weight_grad(model, default_device=store_device).detach()
         b_t_.append(b_l)
     b_matrix = torch.stack(b_t_, dim=0)
-    u, s, vh = torch.linalg.svd(b_matrix, full_matrices=False)
+    u, s, vh = torch.svd_lowrank(b_matrix, q=n_rank, niter=2, M=None)
     v = vh.transpose(-2, -1).conj()
     return s, v
-
 
 if __name__ == "__main__":
 
     seed=42
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    input = torch.zeros((1, 10)).normal_(0, 1)
-    net = SimpleNet()
-    jac = compute_jacobian_single_batch(input, net, 200)
-    u, s, vh = torch.linalg.svd(jac, full_matrices=False)
+    n_input, n_out, n_rank = 10, 100, 50
+    input = torch.zeros((1, n_input)).normal_(0, 1)
+    net = SimpleNet(input_dim=n_input, out_dim=n_out, hidden_dim=50)
+    import time
+    start = time.time()
+    jac = compute_jacobian_single_batch(input, net, n_out)
+    end = time.time()
+    print(end - start)
+    u, s, vh = torch.linalg.svd(jac, full_matrices=True)
     v = vh.transpose(-2, -1).conj()
-    s_approx, v_approx = randomised_SVD_jacobian(input, net)
+    start = time.time()
+    s_approx, v_approx = randomised_SVD_jacobian(input, net, n_rank)
+    end = time.time()
+    print(end - start)
     print('MSE first 20 singular values: {}'.format(torch.mean((s[:len(s_approx)] - s_approx)**2)))
     import matplotlib.pyplot as plt
-    plt.plot(list(range(0, 200)), s);
-    plt.plot(list(range(0, 50)), s_approx); plt.show()
+    plt.plot(list(range(0, n_out)), s);
+    plt.plot(list(range(0, n_rank)), s_approx); plt.show()
+    print(s_approx)
