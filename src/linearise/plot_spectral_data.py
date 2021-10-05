fc30a4933020f49e907be6b260f49b360f97b662
Johannes Leuschner <jleuschn@uni-bremen.de>
2021-10-05 04:14:37 +0200

    update plot spectral data

--- src/linearise/plot_spectral_data.py
+++ src/linearise/plot_spectral_data.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np 
 import yaml
 import matplotlib
@@ -17,10 +18,10 @@ def singular_values_plot(s, labels, colors, line):
     ax.set_yscale('log')
     ax.set_xlabel('# projections', fontsize=12)
     ax.set_ylabel('Magnitude', fontsize=12)
-    lgd = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=2, framealpha=1.)
+    lgd = ax.legend(loc='upper center', bbox_to_anchor=(0.5, 0.05), ncol=2, framealpha=1.)
     fig.savefig('singular_values_comparison_plot.pdf', bbox_extra_artists=(lgd,), bbox_inches='tight')
     
-def singular_vectors_plot(v1, v2, n_cols=4, plot_first_k=None, labels=None, filename=None):
+def singular_vectors_plot(v1, v2, n_cols=4, plot_first_k=None, labels=None, filename=None, reorder_idx=None):
     from matplotlib.ticker import MaxNLocator
     from matplotlib.lines import Line2D
 
@@ -31,12 +32,14 @@ def singular_vectors_plot(v1, v2, n_cols=4, plot_first_k=None, labels=None, file
     n_proj = v1.shape[0] if plot_first_k is None else plot_first_k
     n_rows = n_proj // n_cols
     n_rows += n_proj % n_cols   
+    if reorder_idx is None:
+        reorder_idx = slice(None)
     fig, axs = plt.subplots(n_rows, n_cols, figsize=(25, 15),  facecolor='w', edgecolor='k', constrained_layout=True)
     axs = axs.flatten()
     for i in range(n_proj):
         fct = np.sign(np.dot(v1[i, :], v2[i, :]))
-        axs[i].plot(vec1_len, v1[i, :]*fct, '-', color='#EC2215', alpha=1, linewidth=2.5, label=labels[0])
-        axs[i].plot(vec1_len, v2[i, :], '-', color='#3D78B2', alpha=.75, linewidth=2.5, label=labels[1])
+        axs[i].plot(vec1_len, v1[i, reorder_idx]*fct, '-', color='#EC2215', alpha=1, linewidth=2.5, label=labels[0])
+        axs[i].plot(vec1_len, v2[i, reorder_idx], '-', color='#3D78B2', alpha=.75, linewidth=2.5, label=labels[1])
         axs[i].set_ylabel("$v_{%s}$" % str(i), fontsize=22)
         axs[i].spines['right'].set_visible(False)
         axs[i].spines['top'].set_visible(False)
@@ -46,7 +49,7 @@ def singular_vectors_plot(v1, v2, n_cols=4, plot_first_k=None, labels=None, file
     legend_elements = [Line2D([0], [0], color='#EC2215', alpha=1, linewidth=2.5, label=labels[0]),
                        Line2D([0], [0], color='#3D78B2', alpha=.75, linewidth=2.5, label=labels[1])]
 
-    lgd = fig.legend(handles=legend_elements, loc='upper center', bbox_to_anchor=(0.5, -0.01), ncol=2, framealpha=1., fontsize=22)
+    lgd = fig.legend(handles=legend_elements, loc='upper center', bbox_to_anchor=(0.5, 0.05), ncol=2, framealpha=1., fontsize=22)
     fig.savefig(filename, bbox_extra_artists=(lgd, title, ), bbox_inches='tight')
 
 def proj_diff_params_plot(delta_params_list, v_list, labels, colors, filename):
@@ -72,7 +75,7 @@ def proj_diff_params_plot(delta_params_list, v_list, labels, colors, filename):
     lgd = fig.legend(loc='upper center', bbox_to_anchor=(0.5, -0.01), ncol=2, framealpha=1., fontsize=22)
     fig.savefig(filename, bbox_extra_artists=(lgd, title, ), bbox_inches='tight')
 
-def singular_vectors_3D_plot(v_s, proj_idx, iters, n_rows, n_cols, colors, opacity, labels, iter_labels, filename):
+def singular_vectors_3D_plot(v_s, proj_idx, iters, n_rows, n_cols, colors, opacity, labels, iter_labels, filename, reorder_idx=None):
     from matplotlib.lines import Line2D
 
     params = {'mathtext.default': 'regular' }          
@@ -80,13 +83,15 @@ def singular_vectors_3D_plot(v_s, proj_idx, iters, n_rows, n_cols, colors, opaci
     matplotlib.rcParams.update({'font.size': 12})
 
     len_vec = np.arange(v_s[0].shape[1])
-    fig = matplotlib.pyplot.Figure(figsize=(16, 8))
+    if reorder_idx is None:
+        reorder_idx = slice(None)
+    fig = matplotlib.pyplot.Figure(figsize=(20, 8))
     for i, proj in enumerate(proj_idx):
         ax = fig.add_subplot(n_rows, n_cols, i+1,  projection='3d', facecolor='w')
         for k,  (v, it, cs, alpha) in enumerate(zip(v_s, iters, colors, opacity)):
             fct = np.sign(np.dot(v_s[0][proj, :], v_s[k][proj, :]))
-            ax.plot(len_vec,  v[proj, :]*fct, zs=it, zdir='y', color=cs, alpha=alpha, zorder=10-k, linewidth=0.5)
-        ax.set_zlabel("$v_{%s}$" % str(proj+1), fontsize=16)
+            ax.plot(len_vec,  v[proj, reorder_idx]*fct, zs=it, zdir='y', color=cs, alpha=alpha, zorder=10-k, linewidth=0.5)
+        ax.set_zlabel("$v_{%s}$" % str(proj+1), fontsize=16, labelpad=8. if proj+1==1000 else 4.)
         ax.spines['right'].set_visible(False)
         ax.spines['top'].set_visible(False)
         ax.spines['left'].set_visible(False)
@@ -115,13 +120,19 @@ def singular_vectors_3D_plot(v_s, proj_idx, iters, n_rows, n_cols, colors, opaci
         sqrtN = np.sqrt(x.shape[0])
         return (sqrtN - (np.abs(x).sum()/np.sqrt((x**2).sum())) ) / (sqrtN - 1)
 
-    def add_histogram(ax, range):
+    def add_histogram(ax, slc, xlim_max=None):
+        max_bin = 0.
         for v, it, cs in zip(v_s, iters, colors):
-            ave_hoyer_score = np.mean([compute_hoyer_score(el) for el in v])
-            counts, bins= np.histogram(np.abs(v[:range].flatten()), bins=1000)
+            counts, bins= np.histogram(np.abs(v[slc].flatten()), bins=1000)
+            max_bin = max(bins[-1], max_bin)
             counts = [el if el > 0 else 0 for el in np.log10(counts)]
             ax.bar(bins[:-1], counts, zs=it, zdir='y', color=cs,  width=0.001, alpha=0.8)
-            ax.text(0.15, it, 0, '({:.2f})'.format(ave_hoyer_score), 'x', color=cs, fontsize=10)
+        if xlim_max is None:
+            xlim_max = max_bin
+        for v, it, cs in zip(v_s, iters, colors):
+            ave_hoyer_score = np.mean([compute_hoyer_score(el) for el in v[slc]])
+            # ave_hoyer_score = np.mean([compute_hoyer_score(el) for el in v])
+            ax.text(0.8*xlim_max, it-0.85, 0, '({:.2f})'.format(ave_hoyer_score), 'x', color=cs, fontsize=10)
         
         import matplotlib.ticker as mticker
         def log_tick_formatter(val, pos=None):
@@ -132,7 +143,7 @@ def singular_vectors_3D_plot(v_s, proj_idx, iters, n_rows, n_cols, colors, opaci
         ax.xaxis.set_tick_params(labelsize=12)
         ax.yaxis.set_tick_params(labelsize=12)
         ax.zaxis.set_tick_params(labelsize=12) 
-        ax.set_xlim(0, 0.2)
+        ax.set_xlim(0, xlim_max)
         ax.locator_params(axis='x', nbins=5)      
         ax.set_yticks([np.mean(iters[:len(iters)//2]), np.mean(iters[len(iters)//2:])])
         ax.set_yticklabels(labels, rotation=90)
@@ -147,16 +158,24 @@ def singular_vectors_3D_plot(v_s, proj_idx, iters, n_rows, n_cols, colors, opaci
         ax.grid(None)
     
     ax = fig.add_subplot(n_rows, n_cols, i+2, projection='3d', facecolor='w')
-    add_histogram(ax, range=30)
+    add_histogram(ax, slice(0, 20), xlim_max=0.375)
+    ax.set_title('Histogram (a)', y=0.95)
+    ax = fig.add_subplot(n_rows, n_cols, i+3, projection='3d', facecolor='w')
+    add_histogram(ax, slice(980, 1000), xlim_max=0.375)
+    ax.set_title('Histogram (b)', y=0.95)
+
+    fig.subplots_adjust(top=0.95)
 
     legend_elements = [Line2D([0], [0], color=colors[i], alpha=1, linewidth=2, label=el) for i, el in enumerate(iter_labels)]
-    lgd = fig.legend(handles=legend_elements, loc='upper center', bbox_to_anchor=(0.5, -0.01), ncol=2, framealpha=1., fontsize=14)
+    lgd = fig.legend(handles=legend_elements, loc='upper center', bbox_to_anchor=(0.5, 0.04), ncol=len(iter_labels), framealpha=1., fontsize=14)
     fig.savefig(filename,  bbox_inches='tight')
     
 def main(): 
 
-    with open('./runs.yaml', 'r') as f:
+    with open(os.path.join(os.path.dirname(__file__), 'runs.yaml'), 'r') as f:
         runs = yaml.load(f, Loader=yaml.FullLoader)
+
+    reorder_idx = np.load(os.path.join(os.path.dirname(__file__), 'reorder_idx.npy'))
 
     s, v, params = [], [], []
     for key in runs.keys():
@@ -169,15 +188,16 @@ def main():
     singular_vectors_3D_plot(
         v,
         proj_idx=[
-            0, 1, 2, 3, 29],
-        iters=[1, 2.5, 4, 5.5],
+            0, 1, 2, 3, 499, 999],
+        iters=[1, 3, 5, 8, 10, 12],
         n_rows=2,
-        n_cols=3,
-        colors=['#909090', '#B4B4B4', '#909090', '#B4B4B4'],
-        opacity=[1, 1, 1, 1],
+        n_cols=4,
+        colors=['#084B8D', '#3A9DC7', '#96D6BA', '#084B8D', '#3A9DC7', '#96D6BA'],
+        opacity=[1, 1, 1, 1, 1, 1],
         labels=['EDIP', 'DIP'],
-        iter_labels=['Init', 'Conv.'],
+        iter_labels=['Converged', 'Iteration 100', 'Initial'],
         filename='test.pdf',
+        reorder_idx=reorder_idx
         )
 
     singular_values_plot(
---
+++ src/linearise/reorder_parameter_vector.py
@@ -0,0 +1,142 @@
+import os
+import hydra
+import torch
+import numpy as np
+from omegaconf import DictConfig
+from dataset import get_standard_dataset, get_test_data, get_validation_data
+from deep_image_prior import DeepImagePriorReconstructor
+from utils import randomised_SVD_jacobian
+from torch_utils import parameters_to_vector, list_norm_layers
+
+def unpack_loader(testloader, store_device):
+
+    unpck = [el[1] for el in testloader]
+    fbp = unpck[0].unsqueeze(dim=0)
+    return fbp.to(store_device)
+
+def extract_params(reconstructor, cfg):
+
+    state_dict = torch.load(os.path.join(cfg.path_to_checkpoints,
+                    cfg.filename))
+    reconstructor.model.load_state_dict(state_dict)
+    params = \
+        parameters_to_vector(reconstructor.model.named_parameters(),
+        cfg.skip_layers)
+    return params
+
+def get_named_parameters_sorted(reconstructor, cfg):
+    named_parameters = [(k, v) for k, v in reconstructor.model.named_parameters()
+                        if k not in cfg.skip_layers]
+    named_parameters_sorted = sorted(named_parameters,
+            key=lambda item: ['inc', 'down', 'up', 'outc'].index(
+                    item[0].split('.')[0]))
+    return named_parameters_sorted
+
+def extract_params_sorted(reconstructor, cfg):
+
+    state_dict = torch.load(os.path.join(cfg.path_to_checkpoints,
+                    cfg.filename))
+    reconstructor.model.load_state_dict(state_dict)
+    named_parameters = dict(reconstructor.model.named_parameters())
+    named_parameters_sorted = get_named_parameters_sorted(reconstructor, cfg)
+    params = \
+        parameters_to_vector(named_parameters_sorted,
+        cfg.skip_layers)
+    return params
+
+def get_slices_for_params_in_vector(parameters, cfg):
+    # Pointer for slicing the vector for each parameter
+    slices = {}
+    pointer = 0
+    for name, param in parameters:
+        if name not in cfg.skip_layers:
+            num_param = param.numel()
+            slices[name] = slice(pointer, pointer + num_param)
+
+            # Increment the pointer
+            pointer += num_param
+
+    return slices
+
+def reorder_params_vector(vec, parameters, parameters_sorted, cfg):
+    parameters_list = list(parameters)
+    slices_in_orig = get_slices_for_params_in_vector(parameters_list, cfg)
+    slices_in_sorted = get_slices_for_params_in_vector(parameters_sorted, cfg)
+
+    vec_sorted = np.zeros_like(vec)
+    for name, _ in parameters_list:
+        if name not in cfg.skip_layers:
+            vec_sorted[slices_in_sorted[name]] = vec[slices_in_orig[name]]
+
+    return vec_sorted
+
+@hydra.main(config_path='../cfgs', config_name='config')
+def coordinator(cfg : DictConfig) -> None:
+
+    dataset, ray_trafos = get_standard_dataset(cfg.data.name, cfg.data)
+
+    if cfg.validation_run:
+        if cfg.data.validation_data:
+            dataset_test = get_validation_data(cfg.data.name, cfg.data)
+        else:
+            dataset_test = dataset.create_torch_dataset(
+                fold='validation', reshape=((1,) + dataset.space[0].shape,
+                                            (1,) + dataset.space[1].shape,
+                                            (1,) + dataset.space[1].shape))
+    else:
+        if cfg.data.test_data:
+            dataset_test = get_test_data(cfg.data.name, cfg.data)
+        else:
+            dataset_test = dataset.create_torch_dataset(
+                fold='test', reshape=((1,) + dataset.space[0].shape,
+                                      (1,) + dataset.space[1].shape,
+                                      (1,) + dataset.space[1].shape))
+
+    ray_trafo = {'ray_trafo_module': ray_trafos['ray_trafo_module'],
+                 'reco_space': dataset.space[1],
+                 'observation_space': dataset.space[0]
+                }
+
+    # '''Jacobian approx. with pre-sigmoid network's output'''
+    # if cfg.mdl.arch.use_sigmoid:
+    #     cfg.mdl.arch.use_sigmoid = False
+    #     cfg.mdl.normalize_by_stats = False
+
+    reconstructor = DeepImagePriorReconstructor(**ray_trafo, cfg=cfg.mdl)
+    # '''Exclude GroupNorm & Scale_in/_out params from spectral analsyis & their biases'''
+    # skip_layers = list_norm_layers(reconstructor.model)
+    # if skip_layers is not None:
+    #     cfg.spct.skip_layers += skip_layers # adding group norms. layers 
+
+    store_device = reconstructor.device
+
+    if cfg.mdl.torch_manual_seed:
+        torch.random.manual_seed(cfg.mdl.torch_manual_seed)
+
+    input = unpack_loader(dataset_test, store_device)
+    if cfg.mdl.recon_from_randn:
+        reconstructor.init_model()  # to advance the random seed like in DeepImagePriorReconstructor.reconstruct
+        input = 0.1 * torch.randn(*input.shape).to(store_device)
+    
+    params = extract_params(reconstructor, cfg.spct)
+    params = params.detach().cpu().numpy()
+
+    params_sorted = extract_params_sorted(reconstructor, cfg.spct)
+    params_sorted = params_sorted.detach().cpu().numpy()
+    params_sorted2 = reorder_params_vector(
+            params,
+            reconstructor.model.named_parameters(),
+            get_named_parameters_sorted(reconstructor, cfg.spct),
+            cfg.spct)
+
+    print(np.sum(np.abs(params_sorted2-params_sorted)))
+
+    reorder_idx = reorder_params_vector(
+            np.arange(len(params)),
+            reconstructor.model.named_parameters(),
+            get_named_parameters_sorted(reconstructor, cfg.spct),
+            cfg.spct)
+    np.save('reorder_idx.npy', reorder_idx)
+
+if __name__ == '__main__':
+    coordinator()
