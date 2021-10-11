a166a1c23669ca70178f96f1388cc86529fa7960
Johannes Leuschner <jleuschn@uni-bremen.de>
2021-10-11 14:50:56 +0200

    add images plots for pretraining

---
+++ src/evaluation/scripts/export_pretraining_images.py
@@ -0,0 +1,153 @@
+import os
+import json
+from warnings import warn
+from itertools import islice
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+import yaml
+from evaluation.utils import (
+        get_multirun_cfgs, get_multirun_experiment_names, uses_swa_weights)
+from dataset import get_standard_dataset
+from deep_image_prior import DeepImagePriorReconstructor, PSNR, SSIM
+
+PATH = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+CONFIG_PATH = os.path.join('..', '..', 'cfgs')
+
+OUTPUT_PATH = os.path.join(os.path.dirname(__file__), 'images')
+
+os.makedirs(OUTPUT_PATH, exist_ok=True)
+
+data = 'ellipses_lotus_20'
+# data = 'ellipses_limited_30'
+# data = 'brain_walnut_120'
+# data = 'ellipses_walnut_120'
+
+fold = 'train'
+
+NUM_SAMPLES = 4
+
+OUTPUT_METRICS_PATH = os.path.join(
+        OUTPUT_PATH, '{}_pretraining_{}_metrics.json'.format(data, fold))
+
+with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'runs.yaml'),
+        'r') as f:
+    runs = yaml.load(f, Loader=yaml.FullLoader)
+
+run_spec = {
+    'experiment': 'pretrain_only_fbp',
+}
+
+experiment = run_spec['experiment']
+
+
+available_runs = runs['reconstruction'][data][experiment]
+if run_spec.get('name') is not None:
+    run = next((r for r in available_runs if
+                r.get('name') == run_spec['name']),
+                None)
+    if run is None:
+        raise ValueError('Unknown multirun name "{}" for reconstruction '
+                            'with data={}, experiment={}'.format(
+                                    run_spec['name'], data, experiment))
+else:
+    if len(available_runs) > 1:
+        warn('There are multiple multiruns listed for reconstruction with '
+                'data={}, experiment={}, selecting the first one.'.format(
+                        data, experiment))
+    run = available_runs[0]
+
+if run.get('run_path') is None:
+    raise ValueError('Missing run_path specification for reconstruction '
+                        'with data={}, experiment={}. Please insert it in '
+                        '`runs.yaml`.'.format(data, experiment))
+
+run_path_multirun = os.path.join(PATH, run['run_path'])
+sub_runs = run_spec.get('sub_runs')
+
+cfgs = get_multirun_cfgs(
+        run_path_multirun, sub_runs=sub_runs)
+experiment_names = get_multirun_experiment_names(
+        run_path_multirun, sub_runs=sub_runs)
+if len(cfgs) == 0:
+    raise RuntimeError('No runs found at path "{}", aborting.'.format(
+            run_path_multirun))
+assert all((cfg['data']['name'] == data for cfg in cfgs))
+assert all((cfg['mdl']['load_pretrain_model'] for cfg in cfgs))
+swa = uses_swa_weights(cfgs[0])
+assert all((uses_swa_weights(cfg) == swa for cfg in cfgs))
+assert all((en == experiment for en in experiment_names))
+
+cfg = cfgs[0]
+
+dataset, ray_trafos = get_standard_dataset(cfg.data.name, cfg.data)
+
+ray_trafo = {'ray_trafo_module': ray_trafos['ray_trafo_module'],
+             'reco_space': dataset.space[1],
+             'observation_space': dataset.space[0]
+            }
+
+reconstructor = DeepImagePriorReconstructor(**ray_trafo, cfg=cfg.mdl)
+reconstructor.model.eval()
+
+out_fbps = []
+out_gts = []
+out_recos = []
+out_fbp_metrics = []
+out_reco_metrics = []
+
+torch_dataset = dataset.create_torch_dataset(
+    fold=fold, reshape=((1,) + dataset.space[0].shape,
+                        (1,) + dataset.space[1].shape,
+                        (1,) + dataset.space[1].shape))
+
+data_loader = DataLoader(torch_dataset, batch_size=1, num_workers=0,
+                         shuffle=False)
+
+for k, (_, fbp, gt) in enumerate(islice(data_loader, NUM_SAMPLES)):
+    fbp_np = np.asarray(fbp[0, 0].detach().cpu().numpy())
+    gt_np = np.asarray(gt[0, 0].detach().cpu().numpy())
+
+    path = (cfg.mdl.learned_params_path
+            if cfg.mdl.learned_params_path.endswith('.pt')
+            else cfg.mdl.learned_params_path + '.pt')
+    reconstructor.model.load_state_dict(
+            torch.load(path, map_location=reconstructor.device))
+
+    with torch.no_grad():
+        reco = reconstructor.model(fbp.to(reconstructor.device)).detach().cpu()
+
+    reco_np = np.asarray(reco[0, 0].detach().cpu().numpy())
+
+    out_fbps.append(fbp_np)
+    out_gts.append(gt_np)
+    out_recos.append(reco_np)
+    out_fbp_metrics.append({'psnr': PSNR(fbp_np, gt_np),
+                            'ssim': SSIM(fbp_np, gt_np)})
+    out_reco_metrics.append({'psnr': PSNR(reco_np, gt_np),
+                             'ssim': SSIM(reco_np, gt_np)})
+
+metrics = {
+    'fbp': {
+        'sample_{:d}'.format(k): m for k, m in enumerate(out_fbp_metrics)
+    },
+    'reco': {
+        'sample_{:d}'.format(k): m for k, m in enumerate(out_reco_metrics)
+    },
+}
+
+print('metrics:\n{}'.format(metrics))
+
+if OUTPUT_METRICS_PATH is not None:
+    with open(OUTPUT_METRICS_PATH, 'w') as f:
+        json.dump(metrics, f, indent=4)
+
+
+for k, (fbp, gt, reco) in enumerate(zip(out_fbps, out_gts, out_recos)):
+    fbp_filename = '{}_pretraining_{}_fbp_sample_{:d}'.format(data, fold, k)
+    gt_filename = '{}_pretraining_{}_gt_sample_{:d}'.format(data, fold, k)
+    reco_filename = '{}_pretraining_{}_reco_sample_{:d}'.format(data, fold, k)
+
+    np.save(os.path.join(OUTPUT_PATH, fbp_filename), fbp)
+    np.save(os.path.join(OUTPUT_PATH, gt_filename), gt)
+    np.save(os.path.join(OUTPUT_PATH, reco_filename), reco)
---
+++ src/evaluation/scripts/plot_exported_pretraining_images.py
@@ -0,0 +1,295 @@
+import os
+import json
+from math import ceil
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from mpl_toolkits.axes_grid1.inset_locator import InsetPosition
+
+IMAGES_PATH = os.path.join(os.path.dirname(__file__), 'images')
+
+FIG_PATH = os.path.dirname(__file__)
+
+save_fig = True
+
+data = 'ellipses_lotus_20'
+# data = 'ellipses_limited_30'
+# data = 'brain_walnut_120'
+# data = 'ellipses_walnut_120'
+
+fold = 'train'
+
+METRICS_PATH = os.path.join(
+        IMAGES_PATH, '{}_pretraining_{}_metrics.json'.format(data, fold))
+
+samples = range(1)
+
+CMAP = 'gray'
+
+# possible values for 'type' are
+# 'gt', 'fbp', 'reco'
+
+if data in ['ellipses_lotus_20', 'ellipses_lotus_limited_30',
+            'brain_walnut_120', 'ellipses_walnut_120']:
+
+    images_to_plot = []
+    for k in samples:
+        images_to_plot += [
+            {
+                'type': 'fbp',
+                'show_metrics': True,
+                'sample': k,
+                # 'ylabel': 'Sample {:d}'.format(k),
+            },
+            {
+                'type': 'reco',
+                'experiment': 'pretrain_only_fbp',
+                'show_metrics': True,
+                'sample': k,
+            },
+            {
+                'type': 'gt',
+                'sample': k,
+            },
+        ]
+
+
+plot_settings_dict = {
+    'default': {
+        'nrows': len(samples),
+        'only_top_row_titles': True,
+        'norm_group_inds': 'global',
+        'norm_groups_use_vrange_from_images': {0: [len(images_to_plot)-1]},
+        'colorbars_mode': 'off',
+        'gridspec_kw': {'wspace': 0.025},
+        'pad_inches': 0.,
+    },
+    # 'ellipses_lotus_20': {
+    # },
+    # 'ellipses_lotus_limited_30': {
+    # },
+    # 'brain_walnut_120': {
+    # },
+    # 'ellipses_walnut_120': {
+    # },
+}
+
+
+try:
+    with open(METRICS_PATH, 'r') as f:
+        metrics = json.load(f)
+except FileNotFoundError:
+    metrics = None
+
+def get_filename(image_spec):
+    filename = image_spec.get('filename')
+
+    if filename is None:
+        sample = image_spec['sample']
+        image_type = image_spec['type']
+        if image_type == 'gt':
+            filename = '{}_pretraining_{}_gt_sample_{:d}'.format(
+                    data, fold, sample)
+        elif image_type == 'fbp':
+            filename = '{}_pretraining_{}_fbp_sample_{:d}'.format(
+                    data, fold, sample)
+        elif image_type == 'reco':
+            filename = '{}_pretraining_{}_reco_sample_{:d}'.format(
+                    data, fold, sample)
+        else:
+            raise ValueError(
+                    'Unknown "type" \'{}\' in image spec'.format(image_type))
+
+    if not filename.endswith('.npy'):
+        filename = filename + '.npy'
+
+    return filename
+
+images = [
+    np.load(os.path.join(IMAGES_PATH, get_filename(image_spec)))
+    for image_spec in images_to_plot
+]
+
+plot_settings = plot_settings_dict['default']
+plot_settings.update(plot_settings_dict.get(data, {}))
+
+nrows = plot_settings.get('nrows', len(samples))
+only_top_row_titles = plot_settings.get('only_top_row_titles', False)
+
+norm_group_inds = plot_settings.get('norm_group_inds', 'global')
+if norm_group_inds == 'global':
+    norm_group_inds = [0 if im is not None else None
+                       for im in images]
+elif norm_group_inds == 'individual':
+    norm_group_inds = [i if im is not None else None
+                       for i, im in enumerate(images)]
+else:
+    norm_group_inds = [int(ind) if ind is not None else None
+                       for ind in norm_group_inds]
+
+unique_norm_group_inds = np.unique(
+        [ind for ind in norm_group_inds if ind is not None])
+
+norm_groups_use_vrange_from_images = plot_settings.get(
+        'norm_groups_use_vrange_from_images', {})
+for group_ind in unique_norm_group_inds:
+    norm_groups_use_vrange_from_images.setdefault(
+            group_ind,
+            [i for i, g_ind in enumerate(norm_group_inds)
+             if g_ind == group_ind])
+
+colorbars_mode = plot_settings.get('colorbars_mode', 'individual')
+colorbar_location = plot_settings.get('colorbar_location', 'right')
+use_inset_positioned_colorbar = plot_settings.get(
+        'use_inset_positioned_colorbar', False)
+scilimits = plot_settings.get('scilimits')
+metrics_fontsize = plot_settings.get('metrics_fontsize', 9)
+titlepad = plot_settings.get('titlepad')
+figsize = plot_settings.get('figsize', (9, 3))
+gridspec_kw = plot_settings.get('gridspec_kw', {})
+pad_inches = plot_settings.get('pad_inches', 0.1)
+
+
+def get_title(image_spec):
+    image_type = image_spec['type']
+
+    if image_type == 'gt':
+        title = 'Ground truth'
+    elif image_type == 'fbp':
+        title = 'FBP' if 'lotus' in data else 'FDK'
+    elif image_type == 'reco':
+        title = 'U-Net'
+    else:
+        raise ValueError(
+                'Unknown "type" \'{}\' in image spec'.format(image_type))
+
+    return title
+
+def get_image_metrics(image_spec):
+    sample = image_spec['sample']
+    image_type = image_spec['type']
+    if image_type == 'fbp':
+        image_metrics = metrics['fbp']['sample_{:d}'.format(sample)]
+    elif image_type == 'reco':
+        image_metrics = metrics['reco']['sample_{:d}'.format(sample)]
+    else:
+        raise NotImplementedError
+
+    return image_metrics
+
+vranges_per_norm_group = [
+    (min(np.min(images[i])
+         for i in norm_groups_use_vrange_from_images[group_ind]),
+     max(np.max(images[i])
+         for i in norm_groups_use_vrange_from_images[group_ind]))
+    for group_ind in unique_norm_group_inds
+]
+
+vranges = [vranges_per_norm_group[g_ind] if g_ind is not None else None
+           for g_ind in norm_group_inds]
+
+ncols = ceil(len(images_to_plot) / nrows)
+fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize,
+                        gridspec_kw=gridspec_kw)
+
+image_axes_list = []
+
+for i, (image_spec, image, vrange, ax) in enumerate(zip(
+        images_to_plot, images, vranges, axs.flat)):
+
+    image_type = image_spec['type']
+
+    image_axes = None
+
+    title = get_title(image_spec)
+    if not (only_top_row_titles and i // ncols > 0):
+        ax.set_title(title, pad=titlepad)
+
+    vmin, vmax = vrange
+
+    image_axes = ax.imshow(image.T, cmap=CMAP, vmin=vmin, vmax=vmax,
+                            interpolation='none')
+
+    if image_spec.get('ylabel'):
+        ax.set_ylabel(image_spec['ylabel'])
+
+    if image_spec.get('show_metrics'):
+        image_metrics = get_image_metrics(image_spec)
+        ax.set_xlabel('PSNR: ${:.2f}\,$dB, SSIM: ${:.4f}\,$'.format(
+                image_metrics['psnr'], image_metrics['ssim']),
+                fontsize=metrics_fontsize)
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+
+    image_axes_list.append(image_axes)
+
+def add_inset_positioned_colorbar(image_axes, ax, location='right'):
+    if not isinstance(ax, Axes):
+        ax = np.asarray(ax).ravel()
+        ax = {'right': ax[-1],
+              'bottom': ax[-1],
+              'left': ax[0],
+              'top': ax[0]}[location]
+    ip = InsetPosition(
+            ax,
+            {'right': [1.05, 0., 0.05, 1.],
+             'bottom': [0., -0.1, 1., 0.05],
+             'left': [-0.1, 0., 0.05, 1.],
+             'top': [0., 1.05, 1., 0.05]}[location])
+    cax = Axes(fig, [0., 0., 1., 1.])
+    cax.set_axes_locator(ip)
+    fig.add_axes(cax)
+    orientation = {'right': 'vertical',
+                   'bottom': 'horizontal',
+                   'left': 'vertical',
+                   'top': 'horizontal'}[location]
+    cb = fig.colorbar(image_axes, orientation=orientation, cax=cax)
+    return cb
+
+if colorbars_mode == 'global':
+    assert len(unique_norm_group_inds) == 1  # only global normalization allowed
+    image_axes = next(image_axes for image_axes in image_axes_list
+                      if image_axes is not None)
+    colorbar_fun = (add_inset_positioned_colorbar
+                    if use_inset_positioned_colorbar else
+                    fig.colorbar)
+    cb = colorbar_fun(image_axes, ax=axs, location=colorbar_location)
+    cb.ax.ticklabel_format(scilimits=scilimits)
+elif colorbars_mode == 'individual':
+    for image_axes, ax in zip(image_axes_list, axs.flat):
+        if image_axes is not None:
+            colorbar_fun = (add_inset_positioned_colorbar
+                            if use_inset_positioned_colorbar else
+                            fig.colorbar)
+            cb = colorbar_fun(image_axes, ax=[ax], location=colorbar_location)
+            cb.ax.ticklabel_format(scilimits=scilimits)
+elif colorbars_mode == 'norm_groups':
+    for group_ind in unique_norm_group_inds:
+        image_axes = next(
+                image_axes for image_axes, g_ind in zip(
+                        image_axes_list, norm_group_inds)
+                if g_ind == group_ind)
+        axs_group = [
+                ax for ax, g_ind in zip(axs.flat, norm_group_inds)
+                if g_ind == group_ind]
+        colorbar_fun = (add_inset_positioned_colorbar
+                        if use_inset_positioned_colorbar else
+                        fig.colorbar)
+        cb = colorbar_fun(
+                image_axes, ax=axs_group, location=colorbar_location)
+        if scilimits is not None:
+            cb.ax.ticklabel_format(scilimits=scilimits)
+elif colorbars_mode == 'off':
+    pass
+else:
+    raise NotImplementedError
+
+if save_fig:
+    filename = '{}_pretraining_{}_images.pdf'.format(data, fold)
+    fig.savefig(os.path.join(FIG_PATH, filename),
+                bbox_inches='tight', pad_inches=pad_inches)
+
+plt.show()
